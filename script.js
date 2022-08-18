6f66b6e12f7bd4946e90016bdd47673a5d940ed2
Jacob Lockhart <procoder165@gmail.com>
2022-08-18 10:21:06 -0400

    bullshit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("hello");
+
+var counter = function(z){
+    var result = 0;
+    for (var i = 1; i <= z; i++){
+        result = result + i;
+    }
+
+
+   return result;
+}
+
+console.log(counter(10));
